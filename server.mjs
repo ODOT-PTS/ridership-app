c363842607ddf295e74d3fd4b446c36ed316ef56
Brendan Nee <brendan@blinktag.com>
2021-06-17 09:37:19 -0700

    API endpoints

---
+++ server.mjs
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv'
+
+import express from 'express'
+import next from 'next'
+import { openDb } from 'gtfs'
+
+import { queryRidershipData, queryRoutesAndDirections } from './lib/api.mjs'
+
+dotenv.config()
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(async () => {
+  const server = express()
+
+  const db = await openDb({ sqlitePath: process.env.SQLITE_PATH })
+
+  server.get('/routes', async (req, res) => {
+    const routes = await queryRoutesAndDirections()
+    res.json(routes)
+  })
+
+  server.get('/ridership-data', async (req, res) => {
+    const ridershipData = await queryRidershipData({
+      route_id: req.query.route_id,
+      direction_id: req.query.direction_id,
+      start_date: req.query.start_date,
+      end_date: req.query.end_date
+    })
+    res.json(ridershipData)
+  })
+
+  server.all('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
