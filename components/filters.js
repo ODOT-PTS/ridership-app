db5e5513fab89445403471f44d21a46cf33587dd
Brendan Nee <brendan@blinktag.com>
2021-07-03 20:16:31 -0700

    Move filters to component

---
+++ components/filters.js
@@ -0,0 +1,71 @@
+import DatePicker from 'react-datepicker'
+
+import 'react-datepicker/dist/react-datepicker.css'
+
+import { formatRouteName } from '../lib/formatters.js'
+
+const Filters = ({ filters, setFilters, setGrouping, routes, visualize }) => {
+  return (
+    <>
+      <label className="block">
+        <span className="text-gray-700">Date Range</span>
+
+        <div className="mt-1 block w-full">
+          <DatePicker
+            selectsRange={true}
+            startDate={filters.dateRange[0]}
+            endDate={filters.dateRange[1]}
+            onChange={(update) => {
+              setFilters({ ...filters, dateRange: update });
+            }}
+            isClearable={true}
+          />
+        </div>
+      </label>
+
+      <label className="block">
+        <span className="text-gray-700">Group By</span>
+        <select
+          className="mt-1 block w-full"
+          onChange={event => setGrouping(event.target.value)}
+        >
+          <option value="day">Day</option>
+          <option value="day-of-week">Day of week</option>
+          <option value="day-of-week-type">(Weekday vs Sat vs Sun)</option>
+          <option value="time-of-day">Time of day</option>
+        </select>
+      </label>
+
+      <label className="block">
+        <span className="text-gray-700">Route</span>
+        <select
+          className="mt-1 block w-full"
+          onChange={event => setFilters({ ...filters, routeId: event.target.value })}
+        >
+          <option value="all">All</option>
+          {routes && routes.map(route => <option key={route.route_id} value={route.route_id}>{formatRouteName(route)}</option>)}
+        </select>
+      </label>
+
+      <label className="block">
+        <span className="text-gray-700">Direction</span>
+        <select
+          className="mt-1 block w-full"
+          onChange={event => setFilters({ ...filters, directionId: event.target.value })}
+          value={filters.directionId}
+        >
+          <option value="all">Both</option>
+          <option value="0">0</option>
+          <option value="1">1</option>
+        </select>
+      </label>
+
+      <button
+        className="bg-blue-700 px-5 py-3 text-sm shadow-sm font-medium tracking-wider border text-blue-100 rounded-full hover:shadow-lg hover:bg-blue-800"
+        onClick={() => visualize()}
+      >Visualize</button>
+    </>
+  )
+}
+
+export default Filters
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import { useState, useEffect } from 'react'
 import Head from 'next/head'
-import DatePicker from 'react-datepicker'
 import { DateTime } from 'luxon'
 
-import 'react-datepicker/dist/react-datepicker.css'
 import styles from '../styles/Home.module.css'
 
+import Filters from '../components/filters.js'
 import Results from '../components/results.js'
-import { formatRouteName } from '../lib/formatters.js'
 
 export default function Home() {
   const [filters, setFilters] = useState({
@@ -18,8 +16,6 @@ export default function Home() {
   const [grouping, setGrouping] = useState('day')
   const [routes, setRoutes] = useState()
   const [ridershipData, setRidershipData] = useState()
-
-  const [startDate, endDate] = filters.dateRange
 
   useEffect(async () => {
     try {
@@ -38,15 +34,15 @@ export default function Home() {
 
   const visualize = async () => {
     // Validation
-    if (startDate === null || endDate === null) {
+    if (filters.dateRange[0] === null || filters.dateRange[1] === null) {
       alert('Date range is required')
       return
     }
 
     try {
       const parameters = {
-        start_date: DateTime.fromJSDate(startDate).toISODate(),
-        end_date: DateTime.fromJSDate(endDate).toISODate(),
+        start_date: DateTime.fromJSDate(filters.dateRange[0]).toISODate(),
+        end_date: DateTime.fromJSDate(filters.dateRange[1]).toISODate(),
         route_id: filters.routeId,
         direction_id: filters.directionId
       };
@@ -76,66 +72,21 @@ export default function Home() {
             <h1 className={styles.title}>
               Ridership Data Visualization
             </h1>
-            <label className="block">
-              <span className="text-gray-700">Date Range</span>
-
-              <div className="mt-1 block w-full">
-                <DatePicker
-                  selectsRange={true}
-                  startDate={startDate}
-                  endDate={endDate}
-                  onChange={(update) => {
-                    setFilters({ ...filters, dateRange: update });
-                  }}
-                  isClearable={true}
-                />
-              </div>
-            </label>
-
-            <label className="block">
-              <span className="text-gray-700">Group By</span>
-              <select
-                className="mt-1 block w-full"
-                onChange={event => setGrouping(event.target.value)}
-              >
-                <option value="day">Day</option>
-                <option value="day-of-week">Day of week</option>
-                <option value="day-of-week-type">(Weekday vs Sat vs Sun)</option>
-                <option value="time-of-day">Time of day</option>
-              </select>
-            </label>
-
-            <label className="block">
-              <span className="text-gray-700">Route</span>
-              <select
-                className="mt-1 block w-full"
-                onChange={event => setFilters({ ...filters, routeId: event.target.value })}
-              >
-                <option value="all">All</option>
-                {routes && routes.map(route => <option key={route.route_id} value={route.route_id}>{formatRouteName(route)}</option>)}
-              </select>
-            </label>
-
-            <label className="block">
-              <span className="text-gray-700">Direction</span>
-              <select
-                className="mt-1 block w-full"
-                onChange={event => setFilters({ ...filters, directionId: event.target.value })}
-                value={filters.directionId}
-              >
-                <option value="all">Both</option>
-                <option value="0">0</option>
-                <option value="1">1</option>
-              </select>
-            </label>
-
-            <button
-              className="bg-blue-700 px-5 py-3 text-sm shadow-sm font-medium tracking-wider border text-blue-100 rounded-full hover:shadow-lg hover:bg-blue-800"
-              onClick={() => visualize()}
-            >Visualize</button>
+            <Filters
+              filters={filters}
+              setFilters={setFilters}
+              setGrouping={setGrouping}
+              routes={routes}
+              visualize={visualize}
+            />
           </div>
           <div className="flex-grow-1 w-full ml-5 mt-2">
-            <Results ridershipData={ridershipData} filters={filters} grouping={grouping} routes={routes} />
+            <Results
+              ridershipData={ridershipData}
+              filters={filters}
+              grouping={grouping}
+              routes={routes}
+            />
           </div>
         </div>
       </main>
