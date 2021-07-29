25720954ba1614926da107e0aa11985845f6a0c5
Brendan Nee <brendan@blinktag.com>
2021-07-29 16:25:33 -0700

    Loading graphic

--- components/loading.js
+++ components/loading.js
@@ -5,7 +5,19 @@ const Loading = ({ loading }) => {
     return null
   }
 
-  return <div className={styles.loading}><div className={styles['lds-dual-ring']}></div><div className={styles['loading-text']}>Loading...</div></div>
+  return (
+    <div className={styles.loading}>
+      <div className={styles['loading-text']}>Loading Ridership...</div>
+      <div className={styles.loader}>
+        <div className={styles.train}></div>
+        <div className={styles.track}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Loading
