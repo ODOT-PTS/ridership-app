5dea14d93b19fc5149cf03761932754aeb42c966
Brendan Nee <brendan@blinktag.com>
2021-08-12 12:58:00 -0700

    Update loading

--- components/loading.js
+++ components/loading.js
@@ -1,19 +1,24 @@
 import styles from '../styles/loading.module.css'
 
 const Loading = ({ loading }) => {
-  if (!loading) {
-    return null
-  }
+  // if (!loading) {
+  //   return null
+  // }
 
   return (
     <div className={styles.loading}>
       <div className={styles['loading-text']}>Loading Ridership...</div>
       <div className={styles.loader}>
-        <div className={styles.train}></div>
-        <div className={styles.track}>
-          <span></span>
-          <span></span>
-          <span></span>
+        <div className={styles['sk-grid']}>
+          <div className={styles['sk-grid-cube']}></div>
+          <div className={styles['sk-grid-cube']}></div>
+          <div className={styles['sk-grid-cube']}></div>
+          <div className={styles['sk-grid-cube']}></div>
+          <div className={styles['sk-grid-cube']}></div>
+          <div className={styles['sk-grid-cube']}></div>
+          <div className={styles['sk-grid-cube']}></div>
+          <div className={styles['sk-grid-cube']}></div>
+          <div className={styles['sk-grid-cube']}></div>
         </div>
       </div>
     </div>
